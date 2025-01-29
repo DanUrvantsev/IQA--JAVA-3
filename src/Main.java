09b5ac29e121a558ef9aedcb06a2b9ade8f0f838
Danila <laorant8@gmail.com>
2025-01-29 23:48:40 +0700

    Inition commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //Баланс
+        int balance = 100;
+        //Сумма пополнения
+        int refill = 1100;
+        //Сумма бонусов
+        int bonus;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Баланс:"+(balance+refill+bonus));
+    }
+}
